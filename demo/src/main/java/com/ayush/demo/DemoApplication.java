db14da21894345154b4a3cd458f196aa4d6fed5a
Ayush Singh <ayushsingh444v@gmail.com>
2026-07-13 11:05:05 +0530

    Student Class

---
+++ demo/src/main/java/com/ayush/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.ayush.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		Student student = new Student("Ayush",21);
+		System.out.println(student.getAge());
+		System.out.println(student.getName());
+	}
+
+}
